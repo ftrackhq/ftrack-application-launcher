945c60d7f29fe9a47c52b874c3d144408b82d45d
lorenzo <lorenzo.angeli@ftrack.com>
2020-11-30 15:19:54 +0100

    remove redundant env

--- source/ftrack_application_launcher/__init__.py
+++ source/ftrack_application_launcher/__init__.py
@@ -661,7 +661,6 @@ class ApplicationLauncher(object):
 
         # add legacy_environments
         environment['LOGNAME'] = self.session.api_user
-        environment['FTRACK_APIUSER'] = self.session.api_user
         environment['FTRACK_APIKEY']=self.session.api_key
 
         laucher_dependencies = os.path.normpath(
